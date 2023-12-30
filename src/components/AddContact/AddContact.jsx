17ab51e6f3d1dcf816222caf9b62999a685f6d0e
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-12-30 14:08:40 +0000

    fixed error with contacts add to the list

--- src/components/AddContact/AddContact.jsx
+++ src/components/AddContact/AddContact.jsx
@@ -12,63 +12,66 @@ const AddContact = (props) => {
 
     const handleFormNameChange = (e) => {
         setName(
-            e.target.value ,
+            e.target.value,
         );
     };
 
     const handleFormNumberChange = (e) => {
         setNumber(
-            e.target.value ,
+            e.target.value,
         );
     };
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        
-    
+        props.onFormSubmit({ name, number });
+        resetForm();
+
+    };
+    const resetForm = () => {
+        setName('');
+        setNumber('');
     };
 
 
-        return (
-            <div className={css.main}>
-        
-                <form  onSubmit={handleFormSubmit}>
-                    <label htmlFor=''>
-                    <h1 >Name</h1>
-                        <input
-                       
-                            type="text"
-                            name="name"
-                            value={name}
-                            onChange={handleFormNameChange}
-                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                            title="Name may contain only letters, apostrophe, dash and spaces. 
-                            For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                            required
-                        />
-                    </label>
-                    <label htmlFor=''>
+    return (
+        <div >
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor=''>
+                    <h1>Name</h1>
+                    <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={handleFormNameChange}
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. 
+                        For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                    />
+                </label>
+                <label htmlFor=''>
                     <h1 >Number</h1>
-                        <input 
-                           
-                            type="tel"
-                            name="number"
-                            value={number}
-                            onChange={handleFormNumberChange}
-                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                            required
-                        />
-                    </label>
-                        <button type='submit'>Add contact</button>
-                </form>
-            </div>
-        )
-    }
+                    <input
+                        type="tel"
+                        name="number"
+                        value={number}
+                        onChange={handleFormNumberChange}
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                    />
+                </label>
+                <button type='submit' >Add contact</button>
+            </form>
+        </div>
+    )
+}
+
+
 
 
 AddContact.propTypes = {
-
     onFormSubmit: PropTypes.func.isRequired
 }
 
